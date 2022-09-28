86d44c5b56f03ba18e1042a8176f5996b46fc289
Calvin Yonathan <calvinyonathan51@gmail.com>
2022-09-28 10:27:42 +0700

    added heroku support

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	_ "github.com/joho/godotenv/autoload"
+)
+
+func handler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "hello world",
+	})
+}
+
+func main() {
+	r := gin.Default()
+	r.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"*"},
+	}))
+	r.GET("/", handler)
+	r.Run(":" + os.Getenv("PORT"))
+}
